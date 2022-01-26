afb40932068a4836b75aca84047786baeabab97d
SIluk13 <kodratmhd@gmail.com>
2022-01-26 09:34:57 +0700

    first commit

---
+++ latihan1.js
@@ -0,0 +1,41 @@
+// let name ='Hello World'
+// let answer = 'its Ok'
+
+// let namaDepanSaya = "Muhammad"
+// let namaBelakang = "Kodrat"
+
+// let concat = namaDepanSaya.concat(namaBelakang)
+
+// var x = 20;
+// var y = 21;
+
+// console.log(x < 10 || y > 2)
+// console.log(x < 10 || y > 2)
+
+// var siswa = ["a", "b", "c"]
+// var siswabaru = siswa.push("d")
+// var siswaK = siswa.unshift("z")
+// var siswaM = siswa.pop("d")
+// var siswaM1 = siswa.shift("z")
+// var s= siswa.sort()
+
+// console.log(siswa)
+
+// var gw ={
+//     tinggiBadan : 170,
+//     warnaKulit : "Sawo Matang",
+//     pddk : "S1",
+//     'asal daerah' : "Pasaman"
+
+// }
+// console.log(gw)
+
+var arrayDaftarPeserta =["john Doe", "laki2", "baca buku", 1992]
+var peserta = {
+    nama : arrayDaftarPeserta[0],
+    JenisKelamin : arrayDaftarPeserta[1],
+    hobby : arrayDaftarPeserta[2],
+    tahunLahir : arrayDaftarPeserta[3] 
+
+}
+console.log(peserta)
